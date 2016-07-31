0ca6112acdc18c4091b2a8c0b9282ba9eae501e6
Israel Sotomayor <sotoisra24@gmail.com>
2016-07-31 12:23:46 +0200

    fix: variable name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "mod" {
     name = "${var.name}"
     location = "${var.location}"
-    resource_group_name = "${var.resource_group.name}"
+    resource_group_name = "${var.resource_group_name}"
     public_ip_address_allocation = "${var.public_ip_allocation}"
 }
